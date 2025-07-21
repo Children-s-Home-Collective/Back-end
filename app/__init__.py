da44b88fe7727fedfd8fac2aa825d3f8151729a8
AliceMugure <machariamugure17@gmail.com>
2025-07-21 12:00:37 +0300

    init.py error handling

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app = Flask(__name__)
 
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = 's'  
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''  
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = ''  
 
