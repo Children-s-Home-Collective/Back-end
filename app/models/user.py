db63abd41e20d6264e1a1e50cadf4b6ed057c4fa
chiya morara <chiyamorara@gmail.com>
2025-07-30 18:27:45 +0300

    fixed the migrations issue

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(50), default='user')  
     created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
@@ -21,10 +21,10 @@ class User(db.Model):
     volunteer_roles = db.relationship('Volunteer', lazy=True)
 
     def set_password(self, password):
-        self.password= generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
      return f"<User #{self.id}: {self.name} ({self.role})>"
